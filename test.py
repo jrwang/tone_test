e18416713e0ed3899d5820468b30005ae5ba319c
Jonny Wang <jonnyrwang@gmail.com>
2013-05-10 20:03:43 -0700

    added basic input error-checking, cleaned up code

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-import random
-import sys
+import random, sys, re
 from Tkinter import *
 from tkSnack import *
+import argparse
 
 root = Tkinter.Tk()
 root.withdraw()
@@ -23,9 +23,14 @@ base_freq_l = [
 ]
 
 def split(note):
-    '''helper fn to split a note name into its constituent parts'''
+    '''Helper function to split a note name into its constituent parts'''
     return filter(lambda s: not s.isdigit(), note).upper(), int(filter(lambda s: s.isdigit(), note))
-    
+
+def is_note(string):
+    '''Check if string is well-formed note'''
+    note = re.compile('^[a-gA-G][#b]?([0]|[1-9][0-9]*)$')
+    return re.match(note, string)
+
 def gen_octave(start, end):
     '''Generate the tones to test from, using the C0-B0 octave as a starting point'''
     tones = []
@@ -37,66 +42,76 @@ def gen_octave(start, end):
     return tones
     
 def run(questions=10, invert=False, start_oct=4, end_oct=4):
+    '''Main function that presents the stimuli and prints results'''
     test_l = gen_octave(start_oct, end_oct)
     while questions > len(test_l): # e.g. asking 50 questions about 12 notes
         test_l.append(test_l)
     random.shuffle(test_l)
 
-    results = []
+    right_l, error_l = [], []
     for name, freq in test_l[:questions]:
         if invert:
             right, error = ask_name(name, freq)
         else:
             right, error = ask_freq(name, freq)
-        results.append((right, error))
-    print 'results', results
+        right_l.append(right)
+        error_l.append(error)
+    
+    print "Overall: {}/{} correct".format(right_l.count(True), questions)
+    units = "semitones" if invert else "Hz"
+    print "Off by an average of {:.0f} {}".format(sum(error_l)/len(error_l), units)
+    print 'Full results: ', zip(right_l, error_l)
+
 
 def ask_freq(name, freq):
     '''Ask "what frequency is this musical note"? Returns tuple of correctness (bool) and error (float)'''
-    ans = input("What frequency is {}? (example answers: '440', '440.2') ".format(name))
+    def is_float(s):
+        try:
+            float(s)
+            return True
+        except:
+            return False
+    ans = ''
+    while not is_float(ans):
+        ans = raw_input("What frequency is {}? (example answers: '440', '440.2') ".format(name))
+    ans = float(ans)
     return ans == freq, ans - freq
 
 def ask_name(name, freq):
     '''Ask what letter value is this frequency? Returns tuple of correctness (bool) and error (float). Answer is case-insensitive.'''
     beep(freq)
-    ans = raw_input("What note was just played? (example answers: 'A4', 'a4') ".format(name))
+    ans = ''
+    while not is_note(ans):
+        ans = raw_input("What note was just played? (example answers: 'A4', 'a4') ".format(name))
     return ans.upper() in name, scale_dist(name, ans)
 
 def beep(freq, duration=1, volume=1):
     '''Plays a beep of frequency "freq"'''
-    print "BEEP: freq{}".format(freq) 
-
     s = Sound()
     filt = Filter('generator', 440, 30000, 0.0, 'sine', 8000)
-
     filt.configure(freq)
     s.play(filter=filt,blocking=1)
 
-
 def scale_dist(one, two):
     '''Returns how much higher note two is than note one in semitones'''
-    print "DIFFERENCE: {} {}".format(one.lower(), two.lower())
-    scale = "CDEFGAB" # ignore accidentals for now
+    #print "DIFFERENCE: {} {}".format(one.lower(), two.lower())
+    scale = map(lambda x: split(x[0])[0], base_freq_l) # just the note names
     # separate notes into their parts, e.g. A4, C4 = L1N1, L2N2
     (l1, n1), (l2, n2) = split(one), split(two)
     i1, i2 = scale.index(l1), scale.index(l2)
     
     letter_d = (i2 - i1) % len(scale) # distance of the letter portion of the notes
-    # distance of the number portion: one octave for each difference of 1, fewer one octave when the notes span the gap between octaves (e.g. B4 to C5)
+    # distance of the number portion: one octave for each difference of 1
+    #   , fewer one octave when the notes span the gap between octaves (e.g. B4 to C5)
     number_d = len(scale) * (n2 - n1 - (i1 > i2)) # last part is a boolean evaluated to an int
     #print letter_d, number_d, letter_d+number_d
     return letter_d + number_d
 
 if __name__ == "__main__":
-    print sys.argv
-    invert, questions = False, 10 # defaults
-    if len(sys.argv) > 1:
-        questions=int(sys.argv[1])
-        if len(sys.argv) > 2 and sys.argv[2] == "names":
-            if len(sys.argv) > 3:
-                run(invert=True, questions=questions, start_oct=int(sys.argv[3]), end_oct=int(sys.argv[4]))
-            run(True, questions)
-        run(questions)
-    else:
-        run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q', type=int, dest='questions', default=10)
+    parser.add_argument('--names', action='store_const', const=True, default=False)
+    args = parser.parse_args()
+
+    run(args.questions, args.names)
 
